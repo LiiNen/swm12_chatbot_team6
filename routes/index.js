32f5e6dffb46bb932f76265083fd1cddfc06d4e0
LiiNen <kjeonghoon065@gmail.com>
2021-04-22 11:23:03 +0000

    Rename temp variables

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,36 @@ const mainMenuView = require('../views/mainMenuView');
 const mainMenuController = require('../controllers/mainMenuController');
 menuList = ['menu1', 'menu2', 'menu3', 'menu4'];
 
-mentoringtitle = ['[자유멘토링] 4/24(토) 20:00부터 - 프로젝트 기획 아이디어 브레인스토밍 with 10년차 소마 멘토', '[멘토특강] 2021년 4월 25일(일) 15:00 VAR/AI 및 지정프로젝트 설명, 상담회', '[멘토특강] Technical Writing - 개론&각론, 실습포함, 2021년 5월 1일(토) 12:00~16:00', '[멘토특강] DB Modeling & SQL - #2 - 2021년 5월 3일(월) 18:30~22:30', '[자유멘토링] 프로젝트 아이디어 도출/기획검증 - 2021년 5월 1일(토) 17:00~21:00'];
-mentoringowner = ['김수현', '이민경', '김준범', '김준범', '김준범'];
-mentoringurl = ['https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=525&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=523&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=516&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=517&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=518&menuNo=200046'];
-mentoringdate = ['2021.04.24', '2021.04.28', '2021.05.01', '2021.05.03', '2021.05.01']
-var tempMentoringData = new Array();
+mentoring_title = ['[자유멘토링] 4/24(토) 20:00부터 - 프로젝트 기획 아이디어 브레인스토밍 with 10년차 소마 멘토', '[멘토특강] 2021년 4월 25일(일) 15:00 VAR/AI 및 지정프로젝트 설명, 상담회', '[멘토특강] Technical Writing - 개론&각론, 실습포함, 2021년 5월 1일(토) 12:00~16:00', '[멘토특강] DB Modeling & SQL - #2 - 2021년 5월 3일(월) 18:30~22:30', '[자유멘토링] 프로젝트 아이디어 도출/기획검증 - 2021년 5월 1일(토) 17:00~21:00'];
+mentoring_owner = ['김수현', '이민경', '김준범', '김준범', '김준범'];
+mentoring_url = ['https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=525&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=523&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=516&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=517&menuNo=200046', 'https://swmaestro.org/sw/mypage/mentoLec/view.do?qustnrSn=518&menuNo=200046'];
+mentoring_date = ['2021.04.24', '2021.04.28', '2021.05.01', '2021.05.03', '2021.05.01']
+var temp_mentoring_data = new Array();
 for(i=0;i<5;i++){
-	tempMentoringData.push({});
-	tempMentoringData[i]['id'] = i;
-	tempMentoringData[i]['title'] = mentoringtitle[i];
-	tempMentoringData[i]['owner'] = mentoringowner[i];
-	tempMentoringData[i]['url'] = mentoringurl[i];
-	tempMentoringData[i]['date'] = mentoringdate[i];
+	temp_mentoring_data.push({});
+	temp_mentoring_data[i]['id'] = i;
+	temp_mentoring_data[i]['title'] = mentoring_title[i];
+	temp_mentoring_data[i]['owner'] = mentoring_owner[i];
+	temp_mentoring_data[i]['url'] = mentoring_url[i];
+	temp_mentoring_data[i]['date'] = mentoring_date[i];
 }
 for(i=0;i<5;i++){
-	tempMentoringData.push({});
-	tempMentoringData[5+i]['id'] = 4-i;
-	tempMentoringData[5+i]['title'] = mentoringtitle[4-i];
-	tempMentoringData[5+i]['owner'] = mentoringowner[4-i];
-	tempMentoringData[5+i]['url'] = mentoringurl[4-i];
-	tempMentoringData[5+i]['date'] = mentoringdate[4-i];
+	temp_mentoring_data.push({});
+	temp_mentoring_data[5+i]['id'] = 4-i;
+	temp_mentoring_data[5+i]['title'] = mentoring_title[4-i];
+	temp_mentoring_data[5+i]['owner'] = mentoring_owner[4-i];
+	temp_mentoring_data[5+i]['url'] = mentoring_url[4-i];
+	temp_mentoring_data[5+i]['date'] = mentoring_date[4-i];
 }
 //임의로 총 10개
 //index -1로 초기화, menu1View 호출시마다 1씩 늘려서 5개씩 볼 수 있도록
-tempMentoringIndex = -1;
+mentoring_index = -1;
 function menu1View(conversationId) {
-	tempMentoringIndex++;
-	console.log('tmi: ', tempMentoringIndex);
+	mentoring_index++;
+	console.log('tmi: ', mentoring_index);
 	// 멘토링 리스트 전부 탐색한 이후
-	tempMentoringDataSlice = tempMentoringData.slice(tempMentoringIndex*5, (tempMentoringIndex+1)*5);
-	if (tempMentoringDataSlice.length == 0){
+	temp_mentoring_data_slice = temp_mentoring_data.slice(mentoring_index*5, (mentoring_index+1)*5);
+	if (temp_mentoring_data_slice.length == 0){
 		return {
 			conversationId,
 			text: '멘토링 목록 조회를 마쳤습니다.',
@@ -62,7 +62,7 @@ function menu1View(conversationId) {
 			]	
 		};
 	}
-	const mentoringBlock = tempMentoringDataSlice
+	const mentoringBlock = temp_mentoring_data_slice
 	.flatMap((mentoring_object) => ([{
 		type: 'text',
 		text: `[${mentoring_object['title']}](${mentoring_object['url']})`,
